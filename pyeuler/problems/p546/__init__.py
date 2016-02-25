e953b4ec5ab993017fccd7c1c7d048ae6e5437ac
Weston McNamee <ghost.squadron@gmail.com>
2016-02-24 23:37:59 -0800

    found an optimization, I see more from the logs, need to implement

--- pyeuler/problems/p546/__init__.py
+++ pyeuler/problems/p546/__init__.py
@@ -7,8 +7,13 @@ For example, f5(10) = 18, f7(100) = 1003, and f2(103) = 264830889564.
 
 Find (∑10k=2(∑k=210 fk(1014))) mod (109+7).
 """
+import datetime
 import math
 from pyeuler.common import memoize
+
+done_msg = 'done: i: {} k: {} n: {} new_result: {} result_multiplied_k: {} final_result: {}'
+pop_msg = '<-- pop from unmemoized instance k: {} n: {} final_result: {} <----'
+dive_msg = '-----> dive into unmemoized instance k: {} n: {}'
 
 
 @memoize
@@ -18,20 +23,23 @@ def floor_fraction(i, k):
 
 @memoize
 def floors_revenge(k, n):
-    print('-----> dive into unmemoized instance k: {} n: {}'.format(str(k), str(n)))
+    #print(dive_msg.format(str(k), str(n)))
     if n == 0:
-        print('<-- pop from unmemoized instance k: {} n: {} result: {}'.format(str(k), str(n), '1'))
+        #print(pop_msg.format(str(k), str(n), '1'))
         return 1
 
-    result = 0
-    for i in range(0, n+1):
+    final_result = 0
+    i = 0
+    while i <= n:
         fract = floor_fraction(i, k)
         new_result = floors_revenge(k, fract)
-        result += new_result
-        print('done: k: {} n: {} result: {}'.format(str(k), str(fract), str(new_result)))
+        result_multiplied_k = new_result * k
+        final_result += result_multiplied_k
+        i += k
+        #print(done_msg.format(str(i), str(k), str(fract), str(new_result), str(result_multiplied_k), str(final_result)))
 
-    print('<-- pop from unmemoized instance k: {} n: {} result: {} <----'.format(str(k), str(n), str(result)))
-    return result
+    #print(pop_msg.format(str(k), str(n), str(final_result)))
+    return final_result
 
 
 def run_case(k, n):
@@ -41,6 +49,7 @@ def run_case(k, n):
 
 
 def main():
+    print('started ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     # run_case(5, 10)
     # run_case(7, 100)
     # run_case(2, pow(10, 3))
@@ -52,6 +61,7 @@ def main():
 
     answer %= pow(10, 9) + 7
     print('answer: ' + str(answer))
+    print('done ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 
 if __name__ == '__main__':
     main()
