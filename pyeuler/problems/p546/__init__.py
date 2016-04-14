2deafacf2ea510701110cd71bb791e1115c39fa8
Weston McNamee <ghost.squadron@gmail.com>
2016-04-13 23:09:01 -0700

    so close!!!!

--- pyeuler/problems/p546/__init__.py
+++ pyeuler/problems/p546/__init__.py
@@ -9,6 +9,7 @@ Find (∑10k=2(∑k=210 fk(1014))) mod (109+7).
 """
 import datetime
 import math
+from assertpy import assert_that
 from pyeuler.common import memoize
 
 memo_dict = {}
@@ -17,87 +18,56 @@ depth = 0
 
 
 def floors_revenge(k, n):
-    result = memo_dict.get((k, n))
+    i = 0
+    mult = 1
+    result = 0
+    while i <= n:
+        print("---------------")
+        i_start = i
+        print('mult: {} k: {}'.format(mult, k))
+        extra = (mult - k) * k
+        extra_msg = 'extra: ({} - {k}) * {k} = {}'.format(mult, extra, k=k)
+        if extra < 0:
+            extra = 0
 
-    if result is not None:
-        return result
+        if i == n:
+            k = 1
+            print('adj k: {}'.format(k))
+        elif i + k > n:
+            k = n - i + 1
+            print('adj k: {}'.format(k))
 
-    global depth
-    depth += 2
+        before_extra = k * mult
+        temp_sum = before_extra + extra
+        result += temp_sum
+        i += k
+        print(extra_msg)
+        print('{}-{} = sum of {} + {} = {}'.format(i_start, i - 1, before_extra, extra, temp_sum))
+        print('result: {}'.format(result))
+        mult += 1
 
-    #print((' ' * depth) + '{{ in unmemoized instance of f {k}({n})'.format(k=k, n=n))
-
-    if n == 0:
-        #print((' ' * depth + '}} end f {k}({n}) res {r}'.format(k=k, n=n, r=1)))
-        depth -= 2
-        return 1
-
-    results = []
-    final_result = 0
-    odd_result = 0
-    shortcut_result = 0
-    main_index = n
-
-    depth += 2
-
-    odd_loop_count = (n+1) % k
-    main_index -= odd_loop_count
-    if odd_loop_count > 0:
-        fraction = math.floor(n / k)
-        result = floors_revenge(k, fraction)
-        odd_result = result * odd_loop_count
-        final_result += odd_result
-        #print((' ' * depth + 'odd_ct {c} odd_r {r} starting_final {f}'.format(c=odd_loop_count, r=odd_result, f=1)))
-
-    while main_index >= 0:
-        mem_result = memo_dict.get((k, main_index))
-        if main_index != n and mem_result is not None:
-            #print((' ' * depth + '{k}, {n} already memoized as {r}, shortcut!'.format(k=k, n=main_index, r=mem_result)))
-            shortcut_result = mem_result
-            final_result += mem_result
-            break
-
-        fraction = math.floor(main_index / k)
-        #print((' ' * depth + '{{ working on i {i}, f {k}({f})!'.format(i=main_index, k=k, f=fraction)))
-        result = floors_revenge(k, fraction)
-        new_result = result * k
-        final_result += new_result
-        results.append((main_index, new_result))
-        #print((' ' * depth + '}} done with i {i}, f {k}({f}) = {r} final {fin}'.format(i=main_index, k=k, f=fraction, r=new_result, fin=final_result)))
-        main_index -= k
-    depth -= 2
-
-    mem_result_final = shortcut_result
-    while len(results) > 0:
-        result = results.pop()
-        mem_num = result[0]
-        mem_result_final += result[1]
-        #print((' ' * depth + '  memoizing f {k}({t}) as {r}'.format(k=k, t=mem_num, r=mem_result_final)))
-        memo_dict[(k, mem_num)] = mem_result_final
-
-    memo_dict[(k, n)] = final_result
-    #print((' ' * depth + '}} end f {k}({n}) res {r}'.format(k=k, n=n, r=final_result)))
-    depth -= 2
-    return final_result
+    return result
 
 
-def run_case(k, n):
+def run_case(k, n, expected_result):
     print('-----------------------------')
     print('testing: k = {}, n = {}'.format(str(k), str(n)))
     result = floors_revenge(k, n)
-    print('result: ' + str(result))
+    print('final result: ' + str(result))
     print('-----------------------------')
+
+    assert_that(result).is_equal_to(expected_result)
 
 
 def main():
 
     print('started ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    run_case(5, 10)
-    run_case(7, 100)
-    run_case(2, 61)
-    run_case(2, pow(10, 3))
+    run_case(5, 10, 18)
+    run_case(7, 100, 1003)
+    run_case(2, 61, 264830889564)
+    # run_case(2, pow(10, 3))
 
-    #exit(0)
+    # exit(0)
 
     print('  samples done at ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 
