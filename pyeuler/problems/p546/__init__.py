2e8f9a33d7a4e0f91e91b27036461ff808a5c790
Weston McNamee <ghost.squadron@gmail.com>
2016-04-19 22:36:27 -0700

    leaps and bounds

--- pyeuler/problems/p546/__init__.py
+++ pyeuler/problems/p546/__init__.py
@@ -8,48 +8,79 @@ For example, f5(10) = 18, f7(100) = 1003, and f2(103) = 264830889564.
 Find (∑10k=2(∑k=210 fk(1014))) mod (109+7).
 """
 import datetime
-import math
 from assertpy import assert_that
-from pyeuler.common import memoize
+from sympy import *
 
-memo_dict = {}
+a, b, c, x, y = symbols('a b c x y')
+quadratic_equation_expr = Eq(a * x**2 + b * x + c - y)
 
-depth = 0
+
+def extract_parabolic_values(coords):
+    """
+    returns a tuple with values of (a, b, c)
+    from the equation: y = ax^2 + bx + c
+    :param coords:
+        example: [
+            1,1
+            2,4
+            3,10
+        ]
+    :return: 3-tuple
+    """
+    exprs = []
+    for coor in coords:
+        expr = quadratic_equation_expr.subs([(x, coor[0]), (y, coor[1])])
+        exprs.append(expr)
+
+    return solve(exprs, [a, b, c])
+
+
+def solve_for_y(k, x_val):
+    if k == 2:
+        av, bv, cv = Rational(3, 2), Rational(-3, 2), 1
+    elif k % 2 == 0:
+        av, bv, cv = k/2, -(k-1), 0
+    else:
+        av, bv, cv = Rational(k, 2), Rational(-(k-2)/2), 0
+
+    expr = quadratic_equation_expr.subs([(x, x_val), (a, av), (b, bv), (c, cv)])
+    return solve(expr)[0]
 
 
 def floors_revenge(k, n):
     i = 0
-    mult = 1
-    mult_xtra = 0
+    set_num = 0
     result = 0
+    set_mult = 0
     while i <= n:
-        print("---------------")
         i_start = i
-        print('mult: {} k: {}'.format(mult, k))
-        if mult > k:
-            mult_xtra += 1
-            print('mult_xtra: {}'.format(mult_xtra))
-        adj_mult = mult + mult_xtra
-        print('adj mult: {}'.format(adj_mult))
+        print('--------- {} ---------'.format(i))
+
+        if i % k == 0:
+            print('increasing set_num...')
+            set_num += 1
+            if set_mult % k == 0:
+                print('set_mult BUMP!...')
+                set_mult = solve_for_y(k, set_num)
+            else:
+                print('set_mult incr!...')
+                set_mult += set_num
+
+        print('set: {} set_mult: {}'.format(set_num, set_mult))
 
         if i == n:
-            adj_k = 0
             k = 1
-            print('adj k: {}'.format(adj_k))
+            print('adj k: {}'.format(k))
         elif i + k > n:
-            adj_k = n - i + 1
             k = n - i + 1
-            print('adj k: {} because {i} + {k} > {n}'.format(adj_k, i=i, k=k, n=n))
-        else:
-            adj_k = 0
+            print('adj k: {k} because {i} + {k} > {n}'.format(i=i, k=k, n=n))
 
-        temp_sum = (k * adj_mult) + adj_k
+        temp_sum = k * set_mult
         result += temp_sum
         i += k
         print('{}-{} = sum of {}'.format(i_start, i - 1, temp_sum))
         print('result: {}'.format(result))
-        mult += 1
-
+        print('---------------------')
     return result
 
 
@@ -70,7 +101,7 @@ def main():
     run_case(7, 100, 1003)
     run_case(2, pow(10, 3), 264830889564)
 
-    # exit(0)
+    exit(0)
 
     print('  samples done at ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 
